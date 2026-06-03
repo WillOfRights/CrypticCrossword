ff405320cf5542da6536f7ea94a03409a582be54
William Strong <williamstrong01@gmail.com>
2026-06-02 22:45:12 -0400

    Added validation to CrypticClueExplanation for compatibility of explanations

--- backend/src/main/kotlin/net/deanasdogs/crypticCrossword/modules/puzzle/model/clue/crypticClueExplanation/CrypticClueExplanation.kt
+++ backend/src/main/kotlin/net/deanasdogs/crypticCrossword/modules/puzzle/model/clue/crypticClueExplanation/CrypticClueExplanation.kt
@@ -1,6 +1,7 @@
 package net.deanasdogs.crypticCrossword.modules.puzzle.model.clue.crypticClueExplanation
 
 import kotlinx.serialization.Serializable
+import net.deanasdogs.crypticCrossword.modules.puzzle.domain.CrypticClueExplanationHelper
 
 /**
  * The full explanation of a cryptic clue, can act as a DTO which can be serialized and rendered by the frontend.
@@ -15,6 +16,11 @@ data class CrypticClueExplanation(
         require(explanationParts.isNotEmpty()) { "Explanation cannot have empty explanation parts."}
         require(explanationParts[0].text == clueText) {
             "The first part of the explanation start with the same text as the clue text."
+        }
+        for (i in 0 until explanationParts.size - 1) {
+            require(CrypticClueExplanationHelper.isYieldRelation(explanationParts[i], explanationParts[i + 1])) {
+                "Explanation part $i is not compatible with explanation part ${i+1}."
+            }
         }
     }
 }
