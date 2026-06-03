435eab4bf1ebc1717ac2e05bfbb2297e8b900cb1
William Strong <williamstrong01@gmail.com>
2026-06-02 22:45:51 -0400

    WIP on ExplanationNode for intermediate step to create explanation

---
+++ backend/src/main/kotlin/net/deanasdogs/crypticCrossword/modules/puzzle/model/clue/crypticClueExplanation/ExplanationNode.kt
@@ -0,0 +1,8 @@
+package net.deanasdogs.crypticCrossword.modules.puzzle.model.clue.crypticClueExplanation
+
+/**
+ * The purpose of an `ExplanationNode` is as an intermediate step in constructing a `CrypticClueExplanation`.
+ * `CrypticClue`s fundamentally have a tree structure, where clues combine parts from child clues
+ */
+class ExplanationNode {
+}
